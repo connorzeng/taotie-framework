cbbc021cb4e5f180261b1bc1429f655d7ba48c73
connor_qseng <ganggang334@163.com>
2020-09-08 23:51:54 +0800

    IOC

--- taotie-ioc/src/main/java/com/connor/taotie/ioc/demo/bean/BeanLifeCycleDemo.java
+++ taotie-ioc/src/main/java/com/connor/taotie/ioc/demo/bean/BeanLifeCycleDemo.java
@@ -10,17 +10,28 @@ public class BeanLifeCycleDemo {
 
     public static void main(String[] args) {
 
-        // bean实例化
+        // bean实例化-普通形式
         initializationBean();
+
+        //bean实例化-特殊形式-serviceLoader
+
+
+        //bean实例化-特殊形式-BeanDefinitionRegistry
+
+
+        //bean实例化-特殊形式-AutowireCapableBeanFactory
+
 
     }
 
     private static void initializationBean() {
-        // 通过过静态方法实例化Persion.createPersion
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:META-INF/bean-instantiation-context.xml");
 
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:META-INF/bean-instantiation-context.xml");
+        // 通过过静态方法实例化Persion.createPersion
         System.out.println(context.getBean("persion-static-method"));
+        // 通过实例工厂
         System.out.println(context.getBean("persion-instance-factory"));
+        // 通过BeanFactory
 
     }
 }
