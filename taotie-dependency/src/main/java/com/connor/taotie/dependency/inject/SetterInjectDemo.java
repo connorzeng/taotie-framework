c822de5dff99835b9bd47242eeff8942940f8f0d
connor_qseng <ganggang334@163.com>
2020-09-14 01:01:35 +0800

    IOC

--- taotie-dependency/src/main/java/com/connor/taotie/dependency/inject/SetterInjectDemo.java
+++ taotie-dependency/src/main/java/com/connor/taotie/dependency/inject/SetterInjectDemo.java
@@ -4,8 +4,9 @@ package com.connor.taotie.dependency.inject;
 import com.connor.taotie.dependency.pojo.PersionHolder;
 import com.connor.taotie.ioc.pojo.Persion;
 import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -19,14 +20,19 @@ public class SetterInjectDemo {
     public static void main(String[] args) {
 
         // 使用XML的方式来进行
-        // setterDemoByXmlWay();
+        setterDemoByXmlWay();
 
         // 使用annotation的方式来进行
         setterDemoByannotation();
+
+        // 使用Beandefinition API进行注入
+        setterDemoByBeandefinitionApi();
+
     }
 
 
     private static void setterDemoByXmlWay() {
+        System.out.println("setterDemoByXmlWay---------------------");
         // 声明一个beanFactory
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
         // 读取BeanDefinition的source
@@ -43,6 +49,7 @@ public class SetterInjectDemo {
     }
 
     private static void setterDemoByannotation() {
+        System.out.println("setterDemoByannotation---------------------");
 
         AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext();
         configApplicationContext.register(SetterInjectDemo.class);
@@ -58,12 +65,38 @@ public class SetterInjectDemo {
     }
 
 
+    private static void setterDemoByBeandefinitionApi() {
+        System.out.println("setterDemoByBeandefinitionApi---------------------");
+
+        AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext();
+        // 读取BeanDefinition的source
+        BeanDefinitionReader reader = new XmlBeanDefinitionReader(configApplicationContext);
+        reader.loadBeanDefinitions("classpath:META-INF/dependency-lookup-context.xml");
+
+        // 使用beandifinition API 注入
+        configApplicationContext.registerBeanDefinition("persionHolder", createBeandefinition());
+        configApplicationContext.refresh();
+
+        System.out.println(configApplicationContext.getBean("persionHolder"));
+        configApplicationContext.close();
+    }
+
+    private static BeanDefinition createBeandefinition() {
+
+        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(PersionHolder.class);
+        beanDefinitionBuilder.addPropertyReference("persion", "superPersion");
+
+        return beanDefinitionBuilder.getBeanDefinition();
+    }
+
+
     // 这里非常神奇,
     @Bean
-    public PersionHolder persionHolder(Persion persion){
+    public PersionHolder persionHolder(Persion persion) {
         PersionHolder persionHolder = new PersionHolder();
         persionHolder.setPersion(persion);
         return persionHolder;
     }
+
 
 }
