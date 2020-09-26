3c69acc206aa6bef03c5b8ecf2147457c381beed
connor_qseng <ganggang334@163.com>
2020-09-26 23:28:09 +0800

    spring的注入学习

--- taotie-dependency/src/main/java/com/connor/taotie/dependency/annotation/ConnorAutowired.java
+++ taotie-dependency/src/main/java/com/connor/taotie/dependency/annotation/ConnorAutowired.java
@@ -5,10 +5,11 @@ package com.connor.taotie.dependency.annotation;
  *
  * 自定义的注入标签
  * (会有自定义的xml主表标签,这里还没有涉及)
- *
- *
+ *  继承@Autowired
+ *  参照@Autowired来进行
  */
-public @interface ConnorInject {
+
+public @interface ConnorAutowired {
 
 
 
