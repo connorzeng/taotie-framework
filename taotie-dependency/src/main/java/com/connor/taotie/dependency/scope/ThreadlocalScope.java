183d47064ffd9c088a3de435288f911ad2063728
connor_qseng <ganggang334@163.com>
2020-10-20 23:46:46 +0800

    2020 shenzhen home

--- taotie-dependency/src/main/java/com/connor/taotie/dependency/scope/ThreadlocalScope.java
+++ taotie-dependency/src/main/java/com/connor/taotie/dependency/scope/ThreadlocalScope.java
@@ -4,9 +4,19 @@ import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.config.Scope;
 import org.springframework.core.NamedThreadLocal;
 
+import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 层级关系:
+ * 1. 线程上下文
+ *      threadId: context
+ * 2. 线程context内部key:value
+ *      beanName: bean
+ */
 public class ThreadlocalScope implements Scope {
+
+    public static final String SCOPE_NAME = "threadlocalScope";
 
     /**
      * 命名
@@ -14,43 +24,65 @@ public class ThreadlocalScope implements Scope {
     private static String THRAED_NAME = "threadlocalScope";
 
     /**
+     *
      * 定义一个上下文维护线程上下文的bean
      */
-    private static final NamedThreadLocal<Map<String,Object>> CONTEXT = new NamedThreadLocal<>(THRAED_NAME);
-
-
+    private static final NamedThreadLocal<Map<String, Object>> CONTEXT = new NamedThreadLocal(THRAED_NAME){
+        @Override
+        protected Object initialValue() {
+            return new HashMap<>();
+        }
+    };
 
 
     @Override
     public Object get(String name, ObjectFactory<?> objectFactory) {
 
         // 1. 先从上下文中获取
-        Object obj = CONTEXT.get().get(name);
-        if (obj != null){
+        Object obj = getStringObjectMap().get(name);
+        if (obj != null) {
             return obj;
         }
 
         // 2. 从beanFactory获取,并且在上下文中维护.
         // 特别注意: obj不会无中生有,最终还是从Spring上下文获取
         obj = objectFactory.getObject();
-        if (obj != null){
-            CONTEXT.get().put(name,obj);
+        if (obj != null) {
+            getStringObjectMap().put(name, obj);
         }
         return obj;
     }
 
     /**
+     * 这里要防止NPE
+     *
+     * @return
+     */
+    private Map<String, Object> getStringObjectMap() {
+//        Map<String, Object> stringObjectMap = CONTEXT.get();
+//
+//        if (stringObjectMap == null) {
+//            stringObjectMap = new HashMap<>();
+//            CONTEXT.set(stringObjectMap);
+//        }
+
+        // 已经有初始化方法,这里直接返回
+        return CONTEXT.get();
+    }
+
+    /**
      * 删除上下文的obj
+     *
      * @param name
      * @return
      */
     @Override
     public Object remove(String name) {
 
-        Object obj = CONTEXT.get().get(name);
+        Object obj = getStringObjectMap().get(name);
 
-        if (obj != null){
-            CONTEXT.get().remove(name);
+        if (obj != null) {
+            getStringObjectMap().remove(name);
         }
 
         return obj;
@@ -69,6 +101,7 @@ public class ThreadlocalScope implements Scope {
 
     /**
      * 返回scope的上下文,这里就是返回context
+     *
      * @param key
      * @return
      */
--- taotie-dependency/src/main/java/com/connor/taotie/dependency/scope/ThreadlocalScopeDemo.java
+++ taotie-dependency/src/main/java/com/connor/taotie/dependency/scope/ThreadlocalScopeDemo.java
@@ -1,25 +1,69 @@
 package com.connor.taotie.dependency.scope;
 
 
+import com.connor.taotie.ioc.pojo.Persion;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+import static com.connor.taotie.dependency.scope.ThreadlocalScope.SCOPE_NAME;
+
 /**
  * 自定义scope
  */
 public class ThreadlocalScopeDemo {
 
     // 1.新建一个ThreadlocalScope继承Scope
-        // 在新建类里面维护一个NamedThreadLocal-->ThreadLocal
-        // bean最终还是要在beanFactory里面获取
+    // 在新建类里面维护一个NamedThreadLocal-->ThreadLocal
+    // bean最终还是要在beanFactory里面获取
     // 2.容器启动的时候需要注册这个Scope
-        // BeanFactoryPostProcessor
+    // BeanFactoryPostProcessor
     // 3.多线程执行获取bean
 
 
+    public static void main(String[] args) throws InterruptedException {
 
-    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(ThreadlocalScopeDemo.class);
+        // 这里需要注册SCOPE
+        applicationContext.addBeanFactoryPostProcessor(beanFactory -> {
+            beanFactory.registerScope(SCOPE_NAME, new ThreadlocalScope());
+        });
 
+        // 刷新容器
+        applicationContext.refresh();
 
+        for (int i = 1000; i > 0; i--) {
 
+            Thread thread = new Thread(() -> {
+
+                Persion threadPersion = (Persion) applicationContext.getBean("threadPersion");
+                System.out.println(threadPersion);
+                System.out.println(Thread.currentThread().getId() == threadPersion.getId());
+            });
+
+            thread.start();
+        }
+
+        Thread.sleep(1000);
+        applicationContext.close();
     }
 
+    @Bean
+    public Persion threadPersion1() {
+        Persion persion = new Persion("刚子老师棒耶", 1);
+        persion.setId(Thread.currentThread().getId());
+
+        return persion;
+    }
+
+    @Bean
+    @Scope(value = ThreadlocalScope.SCOPE_NAME)
+    public Persion threadPersion() {
+        Persion persion = new Persion("刚子老师棒耶", 1);
+        persion.setId(Thread.currentThread().getId());
+
+        return persion;
+    }
 
 }
