9566ee1f79ad26a9a2dc63e96fd459775a2db658
connor_qseng <ganggang334@163.com>
2020-10-25 23:39:15 +0800

    2020 shenzhen home

---
+++ taotie-dependency/src/main/java/com/connor/taotie/dependency/lifecycle/PropertiesBeanDefinitionReaderDemo.java
@@ -0,0 +1,17 @@
+package com.connor.taotie.dependency.lifecycle;
+
+
+/**
+ *
+ * 将properties解析成BeanDefinition
+ *
+ */
+public class PropertiesBeanDefinitionReaderDemo {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
