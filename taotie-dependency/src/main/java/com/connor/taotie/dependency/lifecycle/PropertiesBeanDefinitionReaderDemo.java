214bffb91fb97da3f9ef20583477ddfe2d75bbc4
connor_qseng <ganggang334@163.com>
2020-10-26 00:21:57 +0800

    2020 shenzhen home

--- taotie-dependency/src/main/java/com/connor/taotie/dependency/lifecycle/PropertiesBeanDefinitionReaderDemo.java
+++ taotie-dependency/src/main/java/com/connor/taotie/dependency/lifecycle/PropertiesBeanDefinitionReaderDemo.java
@@ -1,17 +1,36 @@
 package com.connor.taotie.dependency.lifecycle;
 
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.EncodedResource;
+
 /**
- *
  * 将properties解析成BeanDefinition
- *
  */
 public class PropertiesBeanDefinitionReaderDemo {
 
 
     public static void main(String[] args) {
 
+        /**
+         *  1. properties beandifition reader
+         *  2. bean factory
+         */
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        PropertiesBeanDefinitionReader reader = new PropertiesBeanDefinitionReader(beanFactory);
+        System.out.println("getBeanDefinitionCount-before:" + beanFactory.getBeanDefinitionCount());
+        reader.loadBeanDefinitions(new EncodedResource(new ClassPathResource("META-INF/persion.properties"), "UTF-8"));
+        System.out.println("getBeanDefinitionCount-after:" + beanFactory.getBeanDefinitionCount());
 
+        // 加载完成后,直接获取;
+        BeanDefinition beanDefinition = beanFactory.getBeanDefinition("persion");
+        Object persion = beanFactory.getBean("persion");
+
+        System.out.println(beanDefinition);
+        System.out.println(persion);
     }
 
 }
