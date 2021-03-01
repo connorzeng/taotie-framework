bee1b97cb26d437171acf1e489cea68ce0abf7aa
connor_qseng <ganggang334@163.com>
2021-03-02 01:34:12 +0800

    add taotie other

---
+++ taotie-other/hello.lua
@@ -0,0 +1,17 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: connor
+-- Date: 2021/2/26
+-- Time: 0:44
+-- To change this template use File | Settings | File Templates.
+--
+
+
+print('hello');
+print('hello');
+
+local hello = {"hello","hello" };
+print(hello[0]);
+
+
+
