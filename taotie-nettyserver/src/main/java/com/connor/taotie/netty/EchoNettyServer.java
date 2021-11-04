5f241e32c21913992eb20f98040b16fc4f925619
connor_qseng <ganggang334@163.com>
2021-11-04 23:17:32 +0800

    netty

---
+++ taotie-nettyserver/src/main/java/com/connor/taotie/netty/EchoNettyServer.java
@@ -0,0 +1,46 @@
+package com.connor.taotie.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class EchoNettyServer {
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        //1. 声明线程组 (一主多从,变异模型)
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);//接收事件socket的  mainReactor
+        EventLoopGroup workerGroup = new NioEventLoopGroup();//默认CPU合数, 处理读写事件.  subReactor/subReactor/subReactor/....多个子subReactor
+
+
+        // 配置参数
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        // 设置线程池
+        serverBootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 100)
+                .handler(new LoggingHandler(LogLevel.DEBUG))// serverSocket channel
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        //给pipeline 设置处理器
+                        //添加handler
+                        socketChannel.pipeline().addLast(new NettyServerHandlerOrg());
+                    }
+                });
+        // 绑定端口  启动服务
+        ChannelFuture channelFuture = serverBootstrap.bind(6668).sync();
+        // 对关闭通道进行监听
+        channelFuture.channel().closeFuture().sync();
+    }
+
+
+}
---
+++ taotie-nettyserver/src/main/java/com/connor/taotie/netty/NettyServerHandlerOrg.java
@@ -0,0 +1,50 @@
+package com.connor.taotie.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.CharsetUtil;
+
+public class NettyServerHandlerOrg  extends ChannelInboundHandlerAdapter {
+
+
+    //ChannelGroup channels = new DefaultChannelGroup();
+
+
+    /**
+     * 读取客户端发送来的数据
+     * @param ctx ChannelHandler的上下文对象 有管道 pipeline 通道 channel 和 请求地址 等信息
+     * @param msg 客户端发送的具体数据
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("客户端请求到了..." + ctx.channel().remoteAddress());
+        ByteBuf buf = (ByteBuf) msg;//这个需要释放. 用SimpleChannelInboundHandler会自动释放
+        System.out.println("客户端发送的数据是:" +buf.toString(CharsetUtil.UTF_8));
+
+    }
+
+    /**
+     * 读取客户端发送数据完成后的方法
+     *    在本方法中可以发送返回的数据
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        // writeAndFlush 是组合方法
+        ctx.writeAndFlush(Unpooled.copiedBuffer("你好啊，客户端....^_^",CharsetUtil.UTF_8));
+    }
+
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+
+        System.out.println("active");
+        super.channelActive(ctx);
+    }
+}
