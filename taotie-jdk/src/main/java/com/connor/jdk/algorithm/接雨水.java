8220b540733a1cf9e1f1776ab973c0dc9f9abc21
connor_qseng <ganggang334@163.com>
2021-12-06 23:39:33 +0800

    接雨水

--- taotie-jdk/src/main/java/com/connor/jdk/algorithm/接雨水.java
+++ taotie-jdk/src/main/java/com/connor/jdk/algorithm/接雨水.java
@@ -17,11 +17,45 @@ public class 接雨水 {
 //        System.out.println(5>>3);//结果是0
 //        System.out.println(-5>>4);//结果是-1,最多移动到-1
 //        System.out.println(-5>>>3);//结果是536870911
-
         int[] height = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-
         int i = trap6(height);
+
+        int j = minStatic(height);
         System.out.println(i);
+    }
+
+    public static int minStatic(int[] height) {
+
+        int sum = 0;
+
+        // 面试官,你的需求是要返回0,还是报错?
+        if (height.length <= 2 || height == null) {
+            return sum;
+        }
+
+        // 栈
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < height.length; i ++) {
+
+            // 当前必须必入栈的柱子高,才能接住雨水
+            // 水池一个台阶while循环一次
+            while (!stack.empty() && height[i] > height[stack.peek()]){
+                int bashH = height[stack.pop()];//地基的尺寸
+                if (stack.empty()){
+                    //相邻的两个矮柱子,不盛水
+                    break;
+                }
+
+                // 计算两堵墙之间的距离
+                int distance = i - stack.peek() - 1;
+                // 水平面的深度(两堵强中最矮的那堵墙)
+                int waterH = Math.min(height[i],height[stack.peek()]);
+
+                sum += distance * (waterH - bashH);
+            }
+            stack.push(i);
+        }
+        return 0;
     }
 
 
